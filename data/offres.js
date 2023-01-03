9dbd1638b24f6d366db301feb9dbceb66bbf5785
EnzoBECHAR <enzo.bechar@gmail.com>
2023-01-03 13:24:59 +0100

    data

---
+++ data/offres.js
@@ -0,0 +1,40 @@
+export const offres = [
+    {
+        name: '1 HR Box',
+        href: '#',
+        price: 590,
+        description: 'All the basics for starting a new business',
+        includedFeatures: [
+            'Potenti felis, in cras at at ligula nunc. ',
+            'Orci neque eget pellentesque.',
+            'Donec mauris sit in eu tincidunt etiam.',
+        ],
+    },
+    {
+        name: '3 HR Box',
+        href: '#',
+        price: 1590,
+        description: 'All the basics for starting a new business',
+        includedFeatures: [
+            'Potenti felis, in cras at at ligula nunc. ',
+            'Orci neque eget pellentesque.',
+            'Donec mauris sit in eu tincidunt etiam.',
+            'Faucibus volutpat magna.',
+        ],
+    },
+    {
+        name: '6 HR Box',
+        href: '#',
+        price: 2950,
+        description: 'All the basics for starting a new business',
+        includedFeatures: [
+            'Potenti felis, in cras at at ligula nunc. ',
+            'Orci neque eget pellentesque.',
+            'Donec mauris sit in eu tincidunt etiam.',
+            'Faucibus volutpat magna.',
+            'Id sed tellus in varius quisque.',
+            'Risus egestas faucibus.',
+            'Risus cursus ullamcorper.',
+        ],
+    },
+]
