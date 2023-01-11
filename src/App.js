6de2f079105309046dbb0dbf4185e7aab137cb8f
EnzoBECHAR <enzo.bechar@gmail.com>
2023-01-11 16:45:34 +0100

    new design and sort

--- src/App.js
+++ src/App.js
@@ -1,25 +1,165 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {modules/* as initModules*/} from "./data/modules";
+import {questions as initQuestions} from "./data/questions";
+import {Vortex} from "react-loader-spinner";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [load, setLoad] = useState(false);
+    const [result, setResult] = useState([]);
+    const [questions, setQuestions] = useState(initQuestions);
+    const [moduleNonRetenu, setModuleNonRetenu] = useState([]);
+    const onSubmit = () => {
+        let inputs = document.querySelectorAll('input[type=radio]:checked');
+        if (inputs.length === Object.keys(questions).length) {
+            setLoad(true)
+            inputs.forEach((input) => {
+                if (input.value.includes("MODULE_") && modules[input.value]) {
+                    console.log(input.value)
+                    console.log(modules[input.value])
+                    if (!result.find(element => element.module === input.value)) {
+                        console.log("push")
+                        let a = result;
+                        a.push(modules[input.value])    // result.push(modules[input.value])
+                        setResult(a)
+                    }
+                }
+            })
+        }else{
+            alert("Veuillez répondre à toutes les questions")
+            return 0
+        }
+        let mo = Object.values(modules).filter(e => !result.includes(e))
+        setModuleNonRetenu(mo)
+        console.log(result)
+    }
+    setTimeout(() => {
+        setLoad(false)
+    }, 1000)
+
+    return (
+        <>
+            {
+                load ?
+                    <div style={{
+                        display: "flex",
+                        justifyContent: 'center',
+                        width: "100%",
+                        alignItems: 'center'
+                    }}>
+                        <p style={{fontSize: "24px", color: "rgb(64, 151, 170)"}}>Merci d'avoir répondu à ce
+                            questionnnaire, nous recherchons les meilleures offres pour vous...</p>
+                        <Vortex
+                            visible={load}
+                            height="80"
+                            width="80"
+                            ariaLabel="vortex-loading"
+                            wrapperStyle={{}}
+                            wrapperClass="vortex-wrapper"
+                            colors={['rgb(64, 151, 170)', 'rgb(105, 198, 203)', 'rgb(51, 84, 109)', 'rgb(64, 151, 170)', 'rgb(105, 198, 203)', 'rgb(51, 84, 109)']}
+                        />
+                    </div>
+                    : result.length === 0 ?
+
+                        <div className="flex justify-center w-full">
+                            <div>
+                                <table className={'w-full border-separate border-spacing-2'}>
+                                    <thead>
+                                    <tr className={'mb-4'}>
+                                        <th className={'text-start'}></th>
+                                        <th>Oui</th>
+                                        <th>Non</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {Object.keys(questions).map((ques, index) => (
+                                        <tr key={index}>
+                                            <td>{questions[ques].question}</td>
+                                            {questions[ques].reponses.map((reponse, index) => (
+                                                <td className={'text-center'} key={index}>
+                                                    <input type="radio" name={ques} value={reponse.target}/>
+                                                </td>
+                                            ))}
+                                        </tr>
+                                    ))}
+                                    </tbody>
+                                </table>
+                                <div className={'flex w-full justify-end  mt-2'}>
+                                    <button className={'bg-blue-500 text-white px-4 py-2 rounded'}
+                                            onClick={() => onSubmit()}>Valider
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        :
+                        <div>
+                            {result.length > 0 ?
+                                <div className={'w-full flex justify-center'}>
+                                    <div>
+                                        <h1 className={'text-center text-2xl mb-5'}>Les HR Box qui vous intéresses</h1>
+                                        <div className={'modules'}>
+                                            {result.map((el, index) => (
+                                                <div key={el.titre}
+                                                     className="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                                                    <div className="flex-shrink-0">
+                                                        <img className="h-48 w-full object-cover" src={el.img} alt=""/>
+                                                    </div>
+                                                    <div className="flex flex-1 flex-col justify-between bg-white p-6">
+                                                        <div className="flex-1">
+                                                            <div className="mt-2 h-[140px] overflow-hidden block">
+                                                                <p className="text-xl font-semibold text-gray-900">{el.titre}</p>
+                                                                <p className="mt-3  text-gray-500">{el.sousTitre}</p>
+                                                            </div>
+                                                        </div>
+                                                        <div className="mt-6 flex items-center">
+                                                            <div className="flex-shrink-0">
+                                                                <p className="text-xl font-semibold text-gray-900">{el.price}Є</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                                : null}
+                            {moduleNonRetenu.length > 0 ?
+                                <div className={'w-full flex justify-center mt-10'}>
+                                    <div>
+                                        <h1 className={'text-center text-2xl mb-5'}>Modules qui peuvents vous intéresser </h1>
+                                        <div className={'modules'}>
+
+                                        {moduleNonRetenu.map((el, index) => (
+                                            <div key={el.titre}
+                                                 className="flex flex-col overflow-hidden rounded-lg shadow-lg">
+                                                <div className="flex-shrink-0">
+                                                    <img className="h-48 w-full object-cover" src={el.img} alt=""/>
+                                                </div>
+                                                <div className="flex flex-1 flex-col justify-between bg-white p-6">
+                                                    <div className="flex-1">
+                                                        <div className="mt-2 h-[140px] overflow-hidden block">
+                                                            <p className="text-xl font-semibold text-gray-900">{el.titre}</p>
+                                                            <p className="mt-3  text-gray-500">{el.sousTitre}</p>
+                                                        </div>
+                                                    </div>
+                                                    <div className="mt-6 flex items-center">
+                                                        <div className="flex-shrink-0">
+                                                            <p className="text-xl font-semibold text-gray-900">{el.price}Є</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))}
+                                        </div>
+                                    </div>
+                                </div>
+                                : null
+                            }
+                        </div>
+            }
+        </>
+
+    );
 }
 
 export default App;
---
+++ src/data/modules.js
@@ -0,0 +1,87 @@
+import logo from '../asset/logo192.png';
+
+export const modules = {
+    MODULE_contrat_travail: {
+        module: 'MODULE_contrat_travail',
+        titre: "Les différents contrats de travail",
+        sousTitre: "Avez-vous utilisé le bon contrat de travail ? Connaissez-vous les différentes formes de contrat ? Les clauses du contrat de travail sont des éléments déterminants.Conditions de vie au travail Les actions pour mettre d&#39;anticiper les risques professionnels, les améliorations des conditions de vie au travail, la gestion du temps, équilibre vie pro/perso Relations collectives de travail Comment signer un accord collectif ? Comment négocier, mettre une place un CSE, organiser les réunions, répondre aux obligations",
+        img: "null",
+        price: 590,
+        color: 'rgb(51, 84, 109)',
+    },
+    MODULE_recrutement: {
+        module: 'MODULE_recrutement',
+        titre: "Recrutement",
+        sousTitre: "Apprenez à préparer vos recrutements, à identifier les compétences nécessaires, les profils, les comportements attendus. Mesurez les couts. Identifiez les étapes clés et les risques.",
+        price: 590,
+        img: logo,
+        color: 'rgb(64, 151, 170)',
+    },
+    MODULE_integration: {
+        module: 'MODULE_integration',
+        titre: "Intégration",
+        sousTitre: "Préparez l'arrivée de votre collaborateur. Créez le lien avant son arrivée. Organisez son parcours intégration. Recueillir les feed- backs de vos arrivants",
+        price: 590,
+        img: "null",
+        color: 'rgb(105, 198, 203, 1.5)',
+    },
+    MODULE_organisation: {
+        module: 'MODULE_organisation',
+        description: "définir le temps de travail, zoom sur le forfait jours, les congès exceptionnels, le télétravail",
+        titre: "Organisation du travail",
+        sousTitre: "Comment définir qui travaille quand, où, combien ?",
+        lien: "",
+        price: 590,
+        img: "null",
+        color: '#e5d449', //rgb(162, 28, 46)', //"#e5d449"', // #c1b33a
+        programme: ['Le projet', 'Le besoin', 'La non-discrimination', 'L\'annonce', 'Le sourcing', 'Le tri des candidatures']
+    },
+    MODULE_developpement_professionnel: {
+        module: 'MODULE_developpement_professionnel',
+        titre: "Développement professionnel",
+        sousTitre: "Développez les compétences, quand comment. ? La formation, le coaching, le mentoring sont des outils efficaces. Comment mesurez les gaps de compétences ?",
+        price: 590,
+        img: "null",
+        color: '#e5d449', //rgb(162, 28, 46)', //"#e5d449"', // #c1b33a
+    },
+    MODULE_condition_vie1: {
+        module: 'MODULE_condition_vie1',
+        titre: "Conditions de vie au travail partie 1",
+        sousTitre: "Les actions pour mettre d'anticiper les risques professionnels, les améliorations des conditions de vie au travail, la gestion du temps, l&#39;équilibre vie pro/perso",
+        img: "null",
+        price: 590,
+        color: 'rgb(51, 84, 109)',
+    },
+    MODULE_condition_vie2: {
+        module: 'MODULE_condition_vie2',
+        titre: "Conditions de vie au travail partie 2",
+        sousTitre: "Les actions pour mettre d'anticiper les risques professionnels, les améliorations des conditions de vie au travail, la gestion du temps, l&#39;équilibre vie pro/perso",
+        img: "null",
+        price: 590,
+        color: 'rgb(51, 84, 109)',
+    },
+    MODULE_relation_collective: {
+        module: 'MODULE_relation_collective',
+        titre: "Relations collectives de travail",
+        sousTitre: "Comment signer un accord collectif ? Comment négocier, mettre une place un CSE, organiser les réunions, répondre aux obligations",
+        img: "null",
+        price: 590,
+        color: 'rgb(51, 84, 109)',
+    },
+    MODULE_paie_renumeration: {
+        module: 'MODULE_paie_renumeration',
+        titre: "MODULE_paie_renumeration",
+        sousTitre: "Comment signer un accord collectif ? Comment négocier, mettre une place un CSE, organiser les réunions, répondre aux obligations",
+        img: "null",
+        price: 590,
+        color: 'rgb(51, 84, 109)',
+    },
+    MODULE_depart_entreprise: {
+        module: 'MODULE_depart_entreprise',
+        titre: "MODULE_depart_entreprise",
+        sousTitre: "Comment signer un accord collectif ? Comment négocier, mettre une place un CSE, organiser les réunions, répondre aux obligations",
+        img: "null",
+        price: 590,
+        color: 'rgb(51, 84, 109)',
+    },
+}
